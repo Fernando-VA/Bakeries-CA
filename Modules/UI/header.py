ace8c0ff6a8d676bf00ffc8f25b0ad263c1bec69
Fernando-VA <0237563@up.edu.mx>
2025-09-22 20:21:00 -0600

    Update header.py

--- Modules/UI/header.py
+++ Modules/UI/header.py
@@ -3,7 +3,7 @@ import streamlit as st
 def show_header(text_title: str):
   # Layout: logo + title side by side
   col1, col2 = st.columns ([1, 6])
-  with coll:
+  with col1:
     st. image("Assets/UP_Logo.jpg", width=200)
     
   with col2:
